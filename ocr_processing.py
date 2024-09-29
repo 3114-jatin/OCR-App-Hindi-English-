145daada7b2198f1db34abc737aa6c386d3e18ee
3114-jatin <64079637+3114-jatin@users.noreply.github.com>
2024-09-30 01:23:27 +0530

    Update ocr_processing.py

--- ocr_processing.py
+++ ocr_processing.py
@@ -12,7 +12,9 @@ def perform_ocr(uploaded_file):
         image = Image.open(uploaded_file)
 
         # Set custom configuration for better Hindi recognition
-        custom_config = r'--oem 3 --psm 6'  # Modify PSM and OEM for improved accuracy
+        #custom_config = r'--oem 3 --psm 6'  # Modify PSM and OEM for improved accuracy
+        custom_config = r'--oem 3 --psm 3 -l hin+eng'
+
 
         # Perform OCR on the image for Hindi (hin) and English (eng)
         extracted_text = pytesseract.image_to_string(image, lang='hin+eng', config=custom_config)
