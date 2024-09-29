29e5a629bd84d4b5977e2f4f1335890d24a2c932
3114-jatin <64079637+3114-jatin@users.noreply.github.com>
2024-09-30 03:14:18 +0530

    Update ocr_processing.py

--- ocr_processing.py
+++ ocr_processing.py
@@ -1,24 +1,35 @@
 import pytesseract
+import cv2
+from PIL import Image
+import numpy as np
 
 # Set the path to the tesseract executable (for Windows)
-#pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR/tesseract.exe'
+# pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 pytesseract.pytesseract.tesseract_cmd = '/usr/bin/tesseract'
 
-from PIL import Image
-import io
+def preprocess_image(uploaded_file):
+    # Open the uploaded image file
+    image = Image.open(uploaded_file)
 
-def perform_ocr(uploaded_file):
+    # Convert image to grayscale
+    image = np.array(image.convert('L'))  # Convert to grayscale (L mode)
+
+    # Apply median filtering to remove noise
+    image = cv2.medianBlur(image, 5)
+
+    # Apply Otsu's thresholding to binarize the image
+    _, image = cv2.threshold(image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+
+    # Return preprocessed image
+    return image
+
+def perform_ocr(image):
     try:
-        # Open the uploaded image file
-        image = Image.open(uploaded_file)
-
         # Set custom configuration for better Hindi recognition
-        #custom_config = r'--oem 3 --psm 6'  # Modify PSM and OEM for improved accuracy
-        #custom_config = r'--oem 3 --psm 3 -l hin+eng'
-         custom_config = r'--oem 3 --psm 6 -l hin+eng'
+        custom_config = r'--oem 3 --psm 6 -l hin+eng'
 
-        # Perform OCR on the image for Hindi (hin) and English (eng)
-        extracted_text = pytesseract.image_to_string(image, lang='hin+eng', config=custom_config)
+        # Perform OCR on the preprocessed image
+        extracted_text = pytesseract.image_to_string(image, config=custom_config)
 
         return extracted_text
 
