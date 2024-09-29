3a0f074b7f0b9c62fdffbfe13d07a0844853d5fe
3114-jatin <64079637+3114-jatin@users.noreply.github.com>
2024-09-30 03:38:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
-from ocr_processing import perform_ocr, preprocess_image
+from ocr_processing import perform_ocr
 from search_function import search_keywords
-from PIL import Image
 
 # Title and description
 st.title('OCR Web Application (Hindi & English)')
@@ -13,11 +12,8 @@ uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
 if uploaded_file:
     st.image(uploaded_file, caption="Uploaded Image", use_column_width=True)
     
-    # Preprocess the uploaded image for better OCR accuracy
-    preprocessed_image = preprocess_image(uploaded_file)
-    
-    # Perform OCR on the preprocessed image
-    extracted_text = perform_ocr(preprocessed_image)
+    # Perform OCR on the uploaded image
+    extracted_text = perform_ocr(uploaded_file)
     
     # Check if OCR returned valid text
     if extracted_text:
